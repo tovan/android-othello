5d92b4ea6e71ab3255ca9e33563fff97219cb64c
TovaN <tovager@gmail.com>
2012-12-04 21:20:05 -0500

    rearranged package structure, classwork

---
+++ src/neuburger/othello/TranslatePointToBox.java
@@ -0,0 +1,14 @@
+package neuburger.othello;
+import android.graphics.Point;
+
+public class TranslatePointToBox {
+
+	
+	public TranslatePointToBox(){
+	}
+	public Point translate(float touchX, float touchY, int boxWidth, int boxHeight){
+		int row = (int)touchX/boxWidth;
+		int column = (int)touchY/boxHeight;
+		return new Point(row, column);
+	}
+}
