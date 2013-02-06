6d06b2b807ef9348e840e0a28a66ec6cfecb93e1
TovaN <tovager@gmail.com>
2013-02-05 21:10:59 -0500

    fixed strategies

--- src/neuburger/othello/EasyStrategy.java
+++ src/neuburger/othello/EasyStrategy.java
@@ -15,17 +15,10 @@ public class EasyStrategy implements Strategy{
 	@Override
 	public GamePiece getNextMove(ArrayList<GamePiece> potentialMoves, int computerColor) {
 		GamePiece bestPiece = null;
-		for (GamePiece piece : potentialMoves) {
-			 if( computerPlayer.isCorner(piece) ){
-				return piece;
-			 }
-			 else{
-				 //return a random move from the list of potential moves
-				 if (potentialMoves.size() > 0){
-					 Random generator = new Random(potentialMoves.size());
-					 bestPiece = potentialMoves.get(generator.nextInt());
-				 }
-			 }
+		 //return a random move from the list of potential moves
+		 if (potentialMoves.size() > 0){
+			 Random generator = new Random();
+			 bestPiece = potentialMoves.get(generator.nextInt(potentialMoves.size()));
 		}
 		return bestPiece;
 	}
--- src/neuburger/othello/MediumStrategy.java
+++ src/neuburger/othello/MediumStrategy.java
@@ -18,17 +18,13 @@ public class MediumStrategy implements Strategy {
 		int playersColor = computerPlayer.getOppositeColor(computerColor);
 		GamePiece bestPiece = null;
 		for (GamePiece piece : potentialMoves) {
-			if (computerPlayer.isCorner(piece)) {
-				bestPiece = piece;
-			} else {
-				// computes 2 tiers of 'predicting'
-				int potentialPiecesGained = computerPlayer.computePiecesGained(piece);
-				piece.setPiecesGained(potentialPiecesGained);
-				ArrayList<GamePiece> potentialPlayerMoves = boardController.getPossibleMoves(playersColor);
-				computerPlayer.resetPiecesGained(potentialPlayerMoves, piece);
-				if (potentialMoves.size() > 0) { //check that a possible move exists
-					bestPiece = computerPlayer.getPieceWithMaxGain(potentialMoves);
-				}
+			// computes 2 tiers of 'predicting'
+			int potentialPiecesGained = computerPlayer.computePiecesGained(piece);
+			piece.setPiecesGained(potentialPiecesGained);
+			ArrayList<GamePiece> potentialPlayerMoves = boardController.getPossibleMoves(playersColor);
+			computerPlayer.resetPiecesGained(potentialPlayerMoves, piece);
+			if (potentialMoves.size() > 0) { //check that a possible move exists
+				bestPiece = computerPlayer.getPieceWithMaxGain(potentialMoves);
 			}
 		}
 		return bestPiece;
